19a45cdb0832837511d6c2030efd66571ac011f3
Darlley <37590954+Darlley@users.noreply.github.com>
2020-03-18 20:36:19 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const http = require('http');
-
 const hostname = '127.0.0.1';
 const port = 3000;
 
@@ -8,7 +7,26 @@ const server = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World');
 });
-
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+var express = require('express')
+var app = express()
+var adminRouter = express.Router()
+
+adminRouter.get('/cursos', function(req, res) {
+    res.send('Uma lista de cursos!')
+})
+adminRouter.post('/cursos', function (req, res) {
+    res.send('Cadastro de novo curso')
+})
+adminRouter.put('/cursos', function (req, res) {
+    res.send('Edição de um curso existente"')
+})
+adminRouter.delete('/cursos', function (req, res) {
+    res.send('Remoção de um curso por ID')
+})
+
+app.use('/', adminRouter)
+app.listen(8000)
